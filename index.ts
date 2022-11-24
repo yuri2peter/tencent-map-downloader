0c22f6a7216f3ba8a6a07eb978c2e62904bba858
yuri2 <yuri2peter@qq.com>
2022-11-24 13:55:14 +0800

    默认合肥点位

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ type Latlgt = [number, number];
 
 async function main() {
   // 腾讯在线地图选点工具 https://lbs.qq.com/getPoint/
-  const p1: Latlgt = [31.57387, 118.483208]; // 格式[纬度, 经度]， 第一个点（左上）的经纬度
-  const p2: Latlgt = [31.565204, 118.499549]; // 格式[纬度, 经度]， 第二个点（右下）的经纬度
+  const p1: Latlgt = [32.02375, 116.974057]; // 格式[纬度, 经度]， 第一个点（左上）的经纬度
+  const p2: Latlgt = [31.695634, 117.635832]; // 格式[纬度, 经度]， 第二个点（右下）的经纬度
 
   await quickStart(p1, p2);
 }
